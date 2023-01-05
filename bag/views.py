87e3eb1442fc6c5930e5c77e16d900503dba9bd8
Yakub Ali <mamuzaan@yahoo.com>
2023-01-05 18:46:40 +0000

    Added toast success message functionality to bag view

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 
 def view_bag(request):
@@ -8,6 +10,8 @@ def view_bag(request):
 
 def add_to_bag(request, item_id):
     """ Add a quentity of the specified product to the shopping bag """
+
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -28,6 +32,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
