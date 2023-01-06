e842ca5c570c8849dafbf44f635ba781f66b2853
Yakub Ali <mamuzaan@yahoo.com>
2023-01-06 18:44:23 +0000

    Added stripe element and css to checkout apps

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MNIpmBelBamBv6rMu11wsjz28GfGBs2iFBarpXeyENjNwQ3VxQ1nkFktzQyPXeYT3dJQVEQ2SwbwT5OjoUkKKaX00YwOQ9kH2',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
