6e4b4022c5415f1610b54df648dfb0cb6d45dcd9
Yakub Ali <mamuzaan@yahoo.com>
2023-01-06 19:44:32 +0000

    Added public key and secret key link in setting.py and export in workspace and add current bag , total in checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
@@ -8,6 +12,10 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's no product in your bag at the moment")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
