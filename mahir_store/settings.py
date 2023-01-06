aafb0c22434ca7d11d3d267f622ba5798e29fb63
Yakub Ali <mamuzaan@yahoo.com>
2023-01-06 10:31:06 +0000

    Added checkout order summary, image template and comment to settings and bag app

--- mahir_store/settings.py
+++ mahir_store/settings.py
@@ -74,9 +74,10 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',  #. required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
             'builtins': [
