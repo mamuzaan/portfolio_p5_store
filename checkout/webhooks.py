6cca99070f153acdcfcf3a4cf17ed89a50cae0d1
Yakub Ali <mamuzaan@yahoo.com>
2023-01-09 22:01:44 +0000

    fix bug to bag template

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -10,18 +10,19 @@ import stripe
 @require_POST
 @csrf_exempt
 def webhook(request):
-    """ Listen for webhooks from Stripe """
+    """Listen for webhooks from Stripe"""
     # Setup
     wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
+    # Get the webhook data and verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
 
     try:
         event = stripe.Webhook.construct_event(
-        payload, sig_header, wh_secret
+            payload, sig_header, wh_secret
         )
     except ValueError as e:
         # Invalid payload
