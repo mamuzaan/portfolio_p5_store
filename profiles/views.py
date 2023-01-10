7c296ee06c903b7c6787f04fd7cbfe60734770a1
Yakub Ali <mamuzaan@yahoo.com>
2023-01-10 22:45:26 +0000

    Update the profile function in profile views

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from .models import UserProfile
 
 
 def profile(request):
     """ Display the user profile """
-
+    profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
-    context = ()
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
