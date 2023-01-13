0ad448e471e0fef86230402e132d072b0a40402b
Yakub Ali <mamuzaan@yahoo.com>
2023-01-13 23:21:07 +0000

    Added edit and delete link to products and product detail template

--- products/views.py
+++ products/views.py
@@ -114,5 +114,5 @@ def delete_product(request, product_id):
     """ Delete a product from the store """
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
-    message.success(request, 'Product deleted!')
+    messages.success(request, 'Product deleted!')
     return redirect(reverse('products'))
