99f810cea93efc80cbd17208d41c2e619eaba4c3
Yakub Ali <mamuzaan@yahoo.com>
2023-01-15 13:15:28 +0000

    Added secret key

--- mahir_store/settings.py
+++ mahir_store/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '4dv1l1d2=+1qw$h99z#687$7dd61i#@+*3q)a1=^0ckt&)d=0u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
