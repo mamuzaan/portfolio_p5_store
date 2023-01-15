b0ef0ef474313960acc76ec6c6bb5a5b8e48043b
Yakub Ali <mamuzaan@yahoo.com>
2023-01-15 21:44:55 +0000

    Fixed profile instance

--- profiles/models.py
+++ profiles/models.py
@@ -27,5 +27,5 @@ class UserProfile(models.Model):
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """ Create or update the user profile """
     if created:
-        instance.userprofile.save()
-    UserProfile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
