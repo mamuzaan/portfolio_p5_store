290813ec7d7eb37303fccfd2c7d628a99e956de4
Yakub Ali <mamuzaan@yahoo.com>
2023-01-15 02:11:38 +0000

    Remove secret key and add debug

--- mahir_store/settings.py
+++ mahir_store/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9jfr9sd3!acu1ekwp$r*%xz$(faymyar+6eht&4scldgjde)l6'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['mahir-store.herokuapp.com', 'localhost']
 
