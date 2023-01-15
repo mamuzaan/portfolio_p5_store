5991bb186100468ca777a9dc3275a16a9183ab70
Yakub Ali <mamuzaan@yahoo.com>
2023-01-15 01:40:08 +0000

    Added ElephantSQL database url and install dj_database_url psycopg2 and added to settings.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mahir_store.wsgi:application
--- mahir_store/settings.py
+++ mahir_store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-9jfr9sd3!acu1ekwp$r*%xz$(faymyar+6eht&4scldgjde)l6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mahir-store.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -117,12 +118,17 @@ WSGI_APPLICATION = 'mahir_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
