7272c3670eafafbec063b94bfd8e7163b46a1628
Yakub Ali <mamuzaan@yahoo.com>
2023-01-17 18:21:58 +0000

    Fix bug

--- mahir_store/urls.py
+++ mahir_store/urls.py
@@ -26,6 +26,9 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    ) + static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'home.views.handler404'
