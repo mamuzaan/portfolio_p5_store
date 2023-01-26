39ed7ed777ba463933fe0cecf70d8d1c56591e5f
Yakub Ali <mamuzaan@yahoo.com>
2023-01-26 13:03:24 +0000

    Debug mode is False

--- mahir_store/settings.py
+++ mahir_store/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# 'DEVELOPMENT' in os.environ
 
-# DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = ['mahir-store.herokuapp.com', 'localhost']
 
