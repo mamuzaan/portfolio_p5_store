553faff475cb73bd31d71996dcb2563897786e53
Yakub Ali <mamuzaan@yahoo.com>
2023-01-26 13:02:05 +0000

    debug mode is True and add class product-img to product template

--- mahir_store/settings.py
+++ mahir_store/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
+# DEBUG = False
+
 ALLOWED_HOSTS = ['mahir-store.herokuapp.com', 'localhost']
 
 
