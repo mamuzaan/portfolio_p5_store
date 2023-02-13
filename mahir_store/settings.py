58f1e4bcd8f200e169f29ce03e5118b9bd20dea2
Yakub Ali <mamuzaan@yahoo.com>
2023-02-13 11:22:05 +0000

    debug is false

--- mahir_store/settings.py
+++ mahir_store/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 # 'DEVELOPMENT' in os.environ
 
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['mahir-store.herokuapp.com', 'localhost']
 
