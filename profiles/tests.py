1356994eb4d276ae99cc2eead2d360823fece6ab
Yakub Ali <mamuzaan@yahoo.com>
2023-03-06 21:17:23 +0000

    Added UserProfile tests in profiles apps models

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from profiles.models import UserProfile
 
-# Create your tests here.
+
+class TestUserProfileSignal(TestCase):
+    def test_create_or_update_user_profile(self):
+        user = User.objects.create(username='testuser', password='testpass')
+        self.assertIsNotNone(user)
+
+        profile = user.userprofile
+        self.assertIsInstance(profile, UserProfile)
+        self.assertEqual(profile.user, user)
+
+        profile.default_phone_number = '+1234567890'
+        profile.default_street_address1 = '123 Main St'
+        profile.default_town_or_city = 'Anytown'
+        profile.default_postcode = '12345'
+        profile.default_country = 'US'
+        profile.save()
+
+        profile.refresh_from_db()
+        self.assertEqual(profile.default_phone_number, '+1234567890')
+        self.assertEqual(profile.default_street_address1, '123 Main St')
+        self.assertEqual(profile.default_town_or_city, 'Anytown')
+        self.assertEqual(profile.default_postcode, '12345')
+        self.assertEqual(profile.default_country, 'US')
