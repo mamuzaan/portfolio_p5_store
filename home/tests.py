ae07ca569ab2517fc793866579eadea9813242d7
Yakub Ali <mamuzaan@yahoo.com>
2023-03-06 20:22:31 +0000

    Added unittest code to tests.py handler404 function from home apps views

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.shortcuts import render
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_handler404(self):
+        response = self.client.get('/invalid/url/')
+        self.assertEqual(response.status_code, 404)
+        self.assertTemplateUsed(response, 'errors/404.html')
