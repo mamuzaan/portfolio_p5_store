b6749756d73ea3add0aec59b9879b6ba8f04b82f
Yakub Ali <mamuzaan@yahoo.com>
2023-03-08 16:21:07 +0000

    Added urls for add and delete comment

--- products/urls.py
+++ products/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
     path('wishlist/<int:product_id>/', views.add_product_to_wishlist, name='add_product_to_wishlist'),
     path('like/<int:product_id>/', views.like_product, name='like_product'),
+    path('comment/<int:product_id>/', views.add_comment, name='comment_product'),
+    path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
 ]
